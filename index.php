4434bb3da5fe979e82676b8bb963740d117acf3d
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2016-10-25 09:56:49 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <!-- Fix Mozilla Firefox warning. -->
+  <!-- Jekyll don't add charset to "content-type" header -->
+  <meta charset="utf-8">
+  <meta http-equiv="refresh"  content="100000">
+
+  <title>Bootstrap-submenu</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://vsn4ik.github.io/bootstrap-submenu/dist/css/bootstrap-submenu.min.css">
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" defer></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" defer></script>
+  <script src="https://vsn4ik.github.io/bootstrap-submenu/dist/js/bootstrap-submenu.js" defer></script>
+  <script src="https://vsn4ik.github.io/bootstrap-submenu/assets/js/docs.js" defer></script>
+
+
+</head>
+
+
+<body>
+  <div class="container-fluid">
+  <div class="row">
+  <div class="col-md-12">
+  <?php include('nav2.php'); ?>
+  </div>
+  </div>
+  </div>
+
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,127 @@
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Inicio</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+       <li class="dropdown">
+        <a tabindex="0" data-toggle="dropdown" data-submenu>
+          Dropdown<span class="caret"></span>
+        </a>
+
+        <ul class="dropdown-menu">
+  <li class="dropdown-submenu">
+  <a tabindex="0">Action</a>
+
+  <ul class="dropdown-menu">
+    <li><a tabindex="0">Sub action</a></li>
+    <li class="dropdown-submenu">
+      <a tabindex="0">Another sub action</a>
+
+      <ul class="dropdown-menu">
+        <li><a tabindex="0">Sub action</a></li>
+        <li><a tabindex="0">Another sub action</a></li>
+        <li><a tabindex="0">Something else here</a></li>
+      </ul>
+    </li>
+    <li><a tabindex="0">Something else here</a></li>
+    <li class="disabled"><a tabindex="-1">Disabled action</a></li>
+    <li class="dropdown-submenu">
+      <a tabindex="0">Another action</a>
+
+      <ul class="dropdown-menu">
+        <li><a tabindex="0">Sub action</a></li>
+        <li><a tabindex="0">Another sub action</a></li>
+        <li><a tabindex="0">Something else here</a></li>
+      </ul>
+    </li>
+  </ul>
+</li>
+<li class="dropdown-header">Dropdown header</li>
+<li class="dropdown-submenu">
+  <a tabindex="0">Another action</a>
+
+  <ul class="dropdown-menu">
+    <li><a tabindex="0">Sub action</a></li>
+    <li><a tabindex="0">Another sub action</a></li>
+    <li><a tabindex="0">Something else here</a></li>
+  </ul>
+</li>
+<li><a tabindex="0">Something else here</a></li>
+<li class="divider"></li>
+<li><a tabindex="0">Separated link</a></li>
+</ul>
+      </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+        <a tabindex="0" data-toggle="dropdown" data-submenu>
+          Dropdown 3<span class="caret"></span>
+        </a>
+
+        <ul class="dropdown-menu">
+  <li class="dropdown-submenu">
+  <a tabindex="0">Action</a>
+
+  <ul class="dropdown-menu">
+    <li><a tabindex="0">Sub action</a></li>
+    <li class="dropdown-submenu">
+      <a tabindex="0">Another sub action</a>
+
+      <ul class="dropdown-menu">
+        <li><a tabindex="0">Sub action</a></li>
+        <li><a tabindex="0">Another sub action</a></li>
+        <li><a tabindex="0">Something else here</a></li>
+      </ul>
+    </li>
+    <li><a tabindex="0">Something else here</a></li>
+    <li class="disabled"><a tabindex="-1">Disabled action</a></li>
+    <li class="dropdown-submenu">
+      <a tabindex="0">Another action</a>
+
+      <ul class="dropdown-menu">
+        <li><a tabindex="0">Sub action</a></li>
+        <li><a tabindex="0">Another sub action</a></li>
+        <li><a tabindex="0">Something else here</a></li>
+      </ul>
+    </li>
+  </ul>
+</li>
+<li class="dropdown-header">Dropdown header</li>
+<li class="dropdown-submenu">
+  <a tabindex="0">Another action</a>
+
+  <ul class="dropdown-menu">
+    <li><a tabindex="0">Sub action</a></li>
+    <li><a tabindex="0">Another sub action</a></li>
+    <li><a tabindex="0">Something else here</a></li>
+  </ul>
+</li>
+<li><a tabindex="0">Something else here</a></li>
+<li class="divider"></li>
+<li><a tabindex="0">Separated link</a></li>
+</ul>
+      </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
